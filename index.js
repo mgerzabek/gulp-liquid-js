a52e2cead181e58b55c7d3c76c4b63a0e0ffad22
Michael Gerzabek <michael.gerzabek@gmail.com>
2022-04-12 21:02:01 +0200

    pretty print

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const Transform = require('stream').Transform
+const { Transform } = require('stream')
 const PluginError = require('plugin-error')
-const {Liquid} = require('liquidjs')
+const { Liquid } = require('liquidjs')
 const replaceExtension = require('replace-ext')
 
 const PLUGIN_NAME = 'gulp-liquidify'
@@ -16,8 +16,9 @@ const defaultOptions = {
   data: {},
 }
 
-module.exports = (options = {}) => {
-  
+module.exports = (options = {}) =>
+{
+
   options = {
     ...defaultOptions,
     ...options
@@ -25,45 +26,59 @@ module.exports = (options = {}) => {
 
   return new Transform({
     objectMode: true,
-    transform(file, enc, callback) {
+    transform(file, enc, callback)
+    {
 
       const engine = new Liquid(options.engine)
 
-      for (const filter in options.filters) {
-        if (Object.prototype.hasOwnProperty.call(options.filters, filter)) {
-          engine.registerFilter(filter, options.filters[filter])
+      for (const filter in options.filters)
+      {
+        if (Object.prototype.hasOwnProperty.call(options.filters, filter))
+        {
+          engine.registerFilter(filter, options.filters[ filter ])
         }
       }
 
-      for (const tag in options.tags) {
-        if (Object.prototype.hasOwnProperty.call(options.tags, tag)) {
-          engine.registerTag(tag, options.tags[tag])
+      for (const tag in options.tags)
+      {
+        if (Object.prototype.hasOwnProperty.call(options.tags, tag))
+        {
+          engine.registerTag(tag, options.tags[ tag ])
         }
       }
 
-      if (options.plugins.length) {
-        for (const plugin of options.plugins) {
+      if (options.plugins.length)
+      {
+        for (const plugin of options.plugins)
+        {
           engine.plugin(plugin)
         }
       }
 
-      if (file.isNull()) {
+      if (file.isNull())
+      {
         return callback(null, file)
       }
 
-      if (file.isStream()) {
+      if (file.isStream())
+      {
         return callback(new PluginError(PLUGIN_NAME, 'Streaming is not supported'))
       }
 
-      if (file.isBuffer()) {
+      if (file.isBuffer())
+      {
         file.path = replaceExtension(file.path, options.ext)
+
         engine.renderFile(file.page.layout, {
           content: file.contents.toString(),
           page: file.page
-        }).then((output) => {
-            file.contents = Buffer.from(output)
-            return callback(null, file)
-          }, err => callback(new PluginError(PLUGIN_NAME, err)))
+        }).then((output) =>
+        {
+          file.contents = Buffer.from(output)
+          return callback(null, file)
+        }).catch((err) => {
+          return callback(new PluginError(PLUGIN_NAME, err))
+        })
       }
 
       return null
